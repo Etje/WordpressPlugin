f7c2cce13dbd54f62facffd45547c6360dd405eb
etje <etien_ruhl@live.nl>
2019-05-14 12:25:28 +0200

    changes

---
+++ snappy-list-plugin.php
@@ -0,0 +1,15 @@
+<?php 
+
+/*
+Plugin Name: snappy List Plugin
+Plugin URI: https://wordpress.altrixmedia.nl/
+Description: The ultimate plugin
+Version: 1.0.0
+Author: Etiën Ruhl
+Author URI: https://www.linkedin.com/in/eti%C3%ABn-ruhl/
+License: GPL2
+License URI: https://www.gnu.org/licenses/licenses.nl.html
+Text Domain: Snappy-list-plugin
+*/
+
+?>
